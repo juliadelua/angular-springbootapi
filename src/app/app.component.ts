abe3bf427907e070a2c07871f5dbdd25e34cd5c8
juliadelua <juliagatoh@gmail.com>
2022-05-13 19:29:00 -0300

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,40 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, VERSION } from '@angular/core';
+
+interface Auth {
+  token: string;
+  username: string;
+  profile: Array<string>;
+}
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
-}
+export class AppComponent {
+  name = 'SpringBooty';
+  site = 'https://glacial-scrubland-11809.herokuapp.com/';
 
+  login = 'ricardo.larguesa@fatec.sp.gov.br';
+  password = '123456';
+
+  auth = null; // objeto para armazenar os dados, variavel de controle, colocar em if para mostrar o form de autenticação ou o site
+
+  constructor(private http: HttpClient) {}
+
+  postLogin() {
+    this.http
+      .post<Auth>(this.site + 'login', {
+        login: this.login,
+        senha: this.password,
+      })
+      .subscribe((data) => {
+        this.auth = data;
+      });
+  }
+
+  postLogout() {
+    this.auth = null;
+  }
+}
